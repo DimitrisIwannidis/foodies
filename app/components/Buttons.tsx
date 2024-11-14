4e239d24dd7c2948ffdfc726a9e9d2f2f9f8bd7a
Dimitris Ioannidis <dimitris.i@kissmybutton.gr>
2024-11-14 20:51:57 +0200

    Progress: implement google and apple wallet

--- app/components/Buttons.tsx
+++ app/components/Buttons.tsx
@@ -1,12 +1,50 @@
 "use client";
-
+import Image from "next/image";
 import React from "react";
+import styled from "styled-components";
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 20px;
+`;
 
 const Buttons = () => {
   return (
     <>
-      <button onClick={() => console.log("Clicked")}>Google Wallet</button>
-      <button onClick={() => console.log("Clicked too")}>Apple Wallet</button>
+      <ButtonWrapper>
+        <button
+          onClick={() => console.log("Clicked")}
+          style={{
+            background: "none",
+            border: "none",
+            padding: "0",
+            cursor: "pointer",
+          }}
+        >
+          <Image
+            src="/Add_to_Google_Wallet_badge.svg.png"
+            alt="Google Wallet"
+            width={150}
+            height={50}
+          />
+        </button>
+        <button
+          onClick={() => console.log("Clicked too")}
+          style={{
+            background: "none",
+            border: "none",
+            padding: "0",
+            cursor: "pointer",
+          }}
+        >
+          <Image
+            src="/add-to-apple-wallet-logo.png"
+            alt="Apple Wallet"
+            width={160}
+            height={50}
+          />
+        </button>
+      </ButtonWrapper>
     </>
   );
 };
--- app/landingpage/page.tsx
+++ app/landingpage/page.tsx
@@ -1,26 +1,54 @@
 "use client";
-
 import React from "react";
 import Buttons from "../components/Buttons";
 import styled from "styled-components";
+import Image from "next/image";
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: #bf4f74;
+const StyledContainer = styled("div")`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
 `;
-const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
+const RightDiv = styled("div")`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100%;
+  overflow: auto;
+  flex: 1.5;
+  @media screen and (max-width: 1040px) {
+    display: none;
+  }
 `;
-
+const LeftDiv = styled("div")`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1.8;
+  height: 100vh;
+  }
+`;
 const FoodPage = () => {
   return (
     <>
-      <Wrapper>
-        <Title>FoodPage</Title>
-        <Buttons />
-      </Wrapper>
+      <StyledContainer>
+        <LeftDiv>
+          <Image
+            src="/restaurant.jpg"
+            alt="Restaurant"
+            layout="fill"
+            objectFit="cover"
+            style={{ position: "absolute", top: 0, left: 0 }}
+          />
+        </LeftDiv>
+        <RightDiv>
+          <Buttons />
+        </RightDiv>
+      </StyledContainer>
     </>
   );
 };
